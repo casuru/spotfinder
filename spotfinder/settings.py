42857582bd1bf79cfb62af9d84c5f87e82fdf7a6
Chris Asuru <casuru@uab.edu>
2020-11-03 07:07:13 -0600

    Add GEOS files to settings

--- spotfinder/settings.py
+++ spotfinder/settings.py
@@ -85,6 +85,8 @@ DATABASES["default"] = dj_database_url.config(
     default = os.environ["DATABASE_URL"]
 )
 
+DATABASES["default"]["ENGINE"] = 'django.contrib.gis.db.backends.postgis'
+
 GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
 GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
 
