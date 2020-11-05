90cbb387e4de7041e48e83260af0a303bfa01cbc
Chris Asuru <casuru@uab.edu>
2020-11-05 07:46:13 -0600

    Add Heroku to valid hosts

--- spotfinder/settings.py
+++ spotfinder/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', ]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'spotfinder-api.herokuapp.com', ]
 
 
 # Application definition
